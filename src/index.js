6060f9d4c76269bb10403e4fa96eea2c03e49c5b
Efrain Hernandez <efrainhg2050@gmail.com>
2021-06-19 23:50:33 -0500

    first component using props

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-const name  = 'Joseph Santana';
-const h1 = <h1>Hello { name } I'm very happy to see you here</h1>
+
+
+const person = {
+    name: 'Joseph',
+    lastName: 'Santana',
+    age: 36,
+    city: 'Madrid'
+};
+
+const Welcome = (props) => {
+    return <p>Welcome { props.name } enjoy this app!</p>;
+}
+
+let h1Class = 'user-wave';
+
+const formatUser = ({name, lastName, age, city }) => `Hello Mr. ${lastName} ${name}.
+I known your age is ${age} and you live in ${city}.`;
 
 ReactDOM.render(
-    h1,
+    <div>
+        <h1 className={ h1Class } id="wave">{formatUser(person)}</h1>
+        <Welcome name={ person.name }/>
+    </div>,
     document.querySelector('#root')
     );
