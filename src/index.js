67ebafc4e3c02b4c79589849cbb39f1345ac20b7
Efrain Hernandez <efrainhg2050@gmail.com>
2021-07-06 21:31:49 -0500

    App js and index for project

--- src/index.js
+++ src/index.js
@@ -1,28 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-
-const person = {
-    name: 'Joseph',
-    lastName: 'Santana',
-    age: 36,
-    city: 'Madrid'
-};
-
-const Welcome = (props) => {
-    return <p>Welcome { props.name } enjoy this app!</p>;
-}
-
-let h1Class = 'user-wave';
-
-const formatUser = ({name, lastName, age, city }) => `Hello Mr. ${lastName} ${name}.
-I known your age is ${age} and you live in ${city}.`;
-
+import { App } from './App';
 ReactDOM.render(
-    <div>
-        <h1 className={ h1Class } id="wave">{formatUser(person)}</h1>
-        <Welcome name={ person.name }/>
-    </div>,
-    document.querySelector('#root')
+    <App />,
+    document.getElementById('root')
     );
